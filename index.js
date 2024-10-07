19a632d2873b6d65bdb90f43188068194b0b016a
f h <falhamad2002@gmail.com>
2024-10-07 10:10:32 +0300

    bmi calculator is done

--- index.js
+++ index.js
@@ -8,4 +8,13 @@ function calculateBMI() {
   console.table({ age, weight, height, male, female });
 
   // You are going to write the code here before the curly braces 👇🏻
+  const heightM = height/100; 
+  var BMI = weight / (heightM^2);
+  alert(BMI);
+
+if (BMI<18.5) alert("Underweight");
+else if (BMI>18.5 && BMI<24.9) alert("Healthy Weight");
+else if (BMI>25.0 && BMI<29.9) alert("Overweight");
+else if (BMI>30) alert("Obesity");
+
 }
