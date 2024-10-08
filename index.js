6cba53793dfce7c3451775dfa92b47fdd9c3aeec
f h <falhamad2002@gmail.com>
2024-10-08 19:34:00 +0300

    fixed the code and solved the challenge

--- index.js
+++ index.js
@@ -8,13 +8,57 @@ function calculateBMI() {
   console.table({ age, weight, height, male, female });
 
   // You are going to write the code here before the curly braces 👇🏻
-  const heightM = height/100; 
-  var BMI = weight / (heightM^2);
+  const heightM = height / 100;
+  const BMI = weight / Math.pow(heightM, 2);
   alert(BMI);
 
-if (BMI<18.5) alert("Underweight");
-else if (BMI>18.5 && BMI<24.9) alert("Healthy Weight");
-else if (BMI>25.0 && BMI<29.9) alert("Overweight");
-else if (BMI>30) alert("Obesity");
-
+  if (age >= 19 && age <= 24) {
+    if (BMI < 19) {
+      alert("Not Healthy (Underweight)");
+    } else if (BMI <= 24) {
+      alert("Healthy Weight");
+    } else {
+      alert("Not Healthy");
+    }
+  } else if (age >= 25 && age <= 34) {
+    if (BMI < 20) {
+      alert("Not Healthy (Underweight)");
+    } else if (BMI <= 25) {
+      alert("Healthy Weight");
+    } else {
+      alert("Not Healthy");
+    }
+  } else if (age >= 35 && age <= 44) {
+    if (BMI < 21) {
+      alert("Not Healthy (Underweight)");
+    } else if (BMI <= 26) {
+      alert("Healthy Weight");
+    } else {
+      alert("Not Healthy");
+    }
+  } else if (age >= 45 && age <= 54) {
+    if (BMI < 22) {
+      alert("Not Healthy (Underweight)");
+    } else if (BMI <= 27) {
+      alert("Healthy Weight");
+    } else {
+      alert("Not Healthy");
+    }
+  } else if (age >= 55 && age <= 64) {
+    if (BMI < 23) {
+      alert("Not Healthy (Underweight)");
+    } else if (BMI <= 28) {
+      alert("Healthy Weight");
+    } else {
+      alert("Not Healthy");
+    }
+  } else if (age >= 65) {
+    if (BMI < 24) {
+      alert("Not Healthy (Underweight)");
+    } else if (BMI <= 29) {
+      alert("Healthy Weight");
+    } else {
+      alert("Not Healthy");
+    }
+  }
 }
